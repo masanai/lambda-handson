a12520764e0970dd6cba540fd5b6ef7f8011b652
Ikuta <masanai@3c22fb0d88b6.ant.amazon.com>
2020-08-28 04:59:38 +0900

    refine

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head><title>Lambdaハンズオン S3アップロード</title><head>
 <body>
     <h1>Lambdaハンズオン S3アップロード</h1>
-    <form action="./upload.php" method="POST">
+    <form action="./upload.php" method="POST" enctype="multipart/form-data">
         <p>バケット名:<input type="text" name="bucket" /></p>
         <p>ファイル:<input type="file" name="f1"/></p>
         <input type="submit" value="Upload" />

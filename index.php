fa14ffaab9f7cdbacaa3caab08c83e374879807c
Ikuta <masanai@3c22fb0d88b6.ant.amazon.com>
2020-08-28 01:37:33 +0900

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>Lambdaハンズオン S3アップロード</title><head>
+<body>
+    <h1>Lambdaハンズオン S3アップロード</h1>
+    <form action="./upload.php" method="POST">
+        <p>バケット名:<input type="text" name="bucket" /></p>
+        <p>ファイル:<input type="file" name="f1"/></p>
+        <input type="submit" value="Upload" />
+    </form>
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,34 @@
+<html>
+<head><title>Lambdaハンズオン S3アップロード完了</title><head>
+<body>
+<?php
+    use Aws\S3\S3Client;
+    use Aws\S3\Exception\S3Exception;
+
+    $bucket = $_POST["bucket"];
+    $key = $_FILES['f1']['name']
+
+    $s3 = new S3Client([
+        'version' => 'latest',
+        'region'  => 'ap-southeast-1'
+    ]);
+
+    try {
+        $fp = fopen(_FILES['f1']['tmp_name'],'rb')
+        // Upload data.
+        $result = $s3->putObject([
+            'Bucket' => $bucket,
+            'Key'    => $key,
+            'Body'   => $fp,
+        ]);
+        fclose()
+        // Print the URL to the object.
+        echo $result['ObjectURL'] . PHP_EOL;
+    } catch (S3Exception $e) {
+        echo $e->getMessage() . PHP_EOL;
+    } finally{
+        fclose($fp)
+    }
+?>
+</body>
+</html>
